a80b9b4c1b773c6bd237020f0cdd6769c586363a
Sam Carlton <thatguy@samuelcarlton.com>
2015-11-29 20:02:18 -0600

    Add iPhone launch screen graphics

--- mobile-config.js
+++ mobile-config.js
@@ -17,16 +17,22 @@ App.info({
 App.icons({
   'iphone': 'public/assets/icon/apple-touch-icon-60x60.png',
   'iphone_2x': 'public/assets/icon/apple-touch-icon-120x120.png',
+  'iphone_3x': 'public/assets/icon/apple-touch-icon-180x180.png',
   // ... more screen sizes and platforms ...
 });
 
-/*
 App.launchScreens({
-  'iphone': 'splash/Default~iphone.png',
-  'iphone_2x': 'splash/Default@2x~iphone.png',
-  // ... more screen sizes and platforms ...
+	'iphone': 'public/assets/launch/Default-portrait~iphone.png',
+	'iphone_2x': 'public/assets/launch/Default-portrait@2x~iphone4.png',
+	'iphone5': 'public/assets/launch/Default-portrait@2x~iphone5.jpg',
+	'iphone6': 'public/assets/launch/Default-portrait@2x~iphone6.png',
+	'iphone6p_portrait': 'public/assets/launch/Default-portrait@3x~iphone6+.png',
+//	'iphone6p_landscape': 'public/assets/launch/',
+//	'ipad_portrait': 'public/assets/launch/Default-Portrait~ipad.png',
+//	'ipad_portrait_2x': 'public/assets/launch/Default-Portrait@2x~ipad.png',
+//	'ipad_landscape': 'public/assets/launch/Default-Landscape~ipad.png',
+//	'ipad_landscape_2x': 'public/assets/launch/Default-Landscape@2x~ipad.png'
 });
-*/
 
 // Set PhoneGap/Cordova preferences
 App.setPreference('BackgroundColor', '0x282828ff');
